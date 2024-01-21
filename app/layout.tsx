0225dd00e478afa77d41dda200ceda8568d20003
eslansa <eslansanchez12345@gmail.com>
2024-01-20 21:36:20 -0500

    creating_routes

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${montserrat.className} antialiased`}>{children}</body>
+      <body className={`${montserrat.className} antialiased`}>
+        {children}
+      </body>
+      <footer className='py-10 flex justify-center items-center'>
+        Make with love for Eslan, learning NextJs 14.
+      </footer>
     </html>
   );
 }
